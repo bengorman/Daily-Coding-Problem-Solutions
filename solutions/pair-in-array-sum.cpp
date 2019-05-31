00bcfe09c726980a91467ab23a6062f0a121408b
Benjamin Gorman <bgorman@Benjamins-MacBook-Pro.local>
2019-05-30 17:19:53 -0700

    added a .gitignore for VSCode files, didn't finish implementing problem 1 yet

---
+++ solutions/pair-in-array-sum.cpp
@@ -0,0 +1,21 @@
+/*
+Daily Coding Problem # 1: Pair in Array Sum
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> arr;
+    string arrayStr;
+    getline(cin, arrayStr);
+    return 0;
+}
